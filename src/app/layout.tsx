c59c7e2b231b9fb47114a8c3d3fce0798478aa20
Fernando Laxi <fernandopegboard@gmail.com>
2024-04-20 12:34:11 -0300

    params config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import KanbanProvider from "@/context/kanbanContext";
 import Modal from "@/components/Modal";
 import ModalTarea from "@/components/ModalTarea";
+import { Suspense } from "react";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -29,16 +30,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${plusJakartaSans.className} bg-kb-gray-light`}
-      >
-        <KanbanProvider>
-          {children}
-          {modal}
-          {modalTarea}
-          <Modal />
-          <ModalTarea />
-        </KanbanProvider>
+      <body className={`${plusJakartaSans.className} bg-kb-gray-light`}>
+        <Suspense>
+          <KanbanProvider>
+            {children}
+            {modal}
+            {modalTarea}
+            <Modal />
+            <ModalTarea />
+          </KanbanProvider>
+        </Suspense>
       </body>
     </html>
   );
