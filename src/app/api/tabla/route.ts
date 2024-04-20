9fb5add81f1f960b5f5c1942820dac7b0c2446ae
Fernando Laxi <fernandopegboard@gmail.com>
2024-04-20 12:03:25 -0300

    nextauth config6

--- src/app/api/tabla/route.ts
+++ src/app/api/tabla/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/libs/prisma";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import authOptions from "@/app/utils/auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET() {
   try {
@@ -17,16 +17,16 @@ export async function GET() {
 export async function POST(request: Request) {
   const { nombre } = await request.json();
 
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
 
   const tablaNueva = await prisma.tabla.create({
     data: {
       nombre: nombre,
-      user: {
+      user:{
         connect: {
-          id: parseInt(session?.id),
-        },
-      },
+          id: parseInt(session?.id)
+        }
+      }
     },
   });
   return NextResponse.json(tablaNueva);
