6fbbace87cde46f7ba4a10258e36ed477df007ee
Fernando Laxi <fernandopegboard@gmail.com>
2024-04-20 11:40:03 -0300

    nextauth config3

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,64 +1,65 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import prisma from "@/libs/prisma";
-import { NextAuthOptions } from "next-auth";
+import { AuthOptions } from "next-auth";
 
+const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "email@correo.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "****",
+        },
+      },
+      async authorize(credentials: any, req): Promise<any> {
+        const data = credentials;
 
-export const authOptions: NextAuthOptions = {
-    providers: [
-      CredentialsProvider({
-        name: "credentials",
-        credentials: {
-          email: {
-            label: "Email",
-            type: "email",
-            placeholder: "email@correo.com",
+        const usuarioEncontrado = await prisma.user.findUnique({
+          where: {
+            email: data.email,
           },
-          password: {
-            label: "Password",
-            type: "password",
-            placeholder: "****",
-          },
-        },
-        async authorize(credentials: any, req): Promise<any> {
-          const data = credentials;
-  
-          const usuarioEncontrado = await prisma.user.findUnique({
-            where: {
-              email: data.email,
-            },
-          });
-  
-          if (!usuarioEncontrado) throw new Error("Credenciales Invalidas");
-  
-          const passwordValido = await bcrypt.compare(
-            data.password,
-            usuarioEncontrado.password
-          );
-          if (!passwordValido) throw new Error("Credenciales Invalidas");
-  
-          return {
-            id: usuarioEncontrado.id,
-            email: usuarioEncontrado.email,
-          };
-        },
-      }),
-    ],
-    callbacks: {
-      async jwt({ token, user }) {
-        if (user) {
-          token.id = user.id;
-        }
-        return token;
+        });
+
+        if (!usuarioEncontrado) throw new Error("Credenciales Invalidas");
+
+        const passwordValido = await bcrypt.compare(
+          data.password,
+          usuarioEncontrado.password
+        );
+        if (!passwordValido) throw new Error("Credenciales Invalidas");
+
+        return {
+          id: usuarioEncontrado.id,
+          email: usuarioEncontrado.email,
+        };
       },
-      async session({ session, token, user }) {
-        if (token) {
-          session.id = token.id;
-        }
-        return session;
-      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
     },
-    pages: {
-      signIn: "/auth/login",
+    async session({ session, token, user }) {
+      if (token) {
+        session.id = token.id;
+      }
+      return session;
     },
-  };
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+};
+
+export default authOptions;
